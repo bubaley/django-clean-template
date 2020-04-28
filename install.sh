3a92c3135a03ae8a4f841b364b9d8eb695c19667
bubaley <bubaley.fu@gmail.com>
2020-04-28 11:42:00 +0000

    build

--- install.sh
+++ install.sh
@@ -1,22 +1,53 @@
 #!/bin/bash
-base_python_interpreter=""
+base_python=""
+default_python="/home/www/.python/bin/python3.8"
 project_domain=""
 project_path=`pwd`
+basedir=`basedir $(pwd)`
+pulled_project=""
 
-read -p "Python interpreter: " base_python_interpreter
+echo $project_path
+
+read -p "Python interpreter (default=$default_python): " base_python
+if [ "$base_python" = "" ]
+then
+base_python=$default_python
+fi
+
 read -p "Your domain without protocol (for example, google.com): " project_domain
-`$base_python_interpreter -m venv env`
+read -p "Did you pull your project to src? (y/n): " pulled_project
+
+`$base_python -m venv env`
 source env/bin/activate
 pip install -U pip
-pip install -r requirements.txt
 
-sed -i "s~dbms_template_path~$project_path~g" nginx/site.conf systemd/gunicorn.service
-sed -i "s~dbms_template_domain~$project_domain~g" nginx/site.conf src/config/settings.py
+if [ "$pulled_project"="y" ]
+then
+pip install -r src/requirements.txt
+fi
 
-sudo ln -s $project_path/nginx/site.conf /etc/nginx/sites-enabled/
-sudo ln -s $project_path/systemd/gunicorn.service /etc/systemd/system/
+cp nginx/site.conf nginx/$basedir.conf
+cp systemd/gunicorn.service systemd/gu-$basedir.conf
+
+sed -i "s~template_domain~$project_domain~g" nginx/$basedir.conf
+sed -i "s~template_path~$project_path~g" nginx/$basedir.conf systemd/gu-$basdir.service
+
+
+if [ -e "/etc/nginx/sites-enabled/$basedir.conf" ]
+then
+else 
+sudo rm /etc/nginx/sites-enabled/$basedir.conf
+fi
+if [ -e "/etc/systemd/system/gu-$basedir.service" ]
+then
+else 
+sudo rm /etc/nginx/sites-enabled/$basedir.conf
+fi
+
+sudo ln -s $project_path/nginx/$basedir.conf /etc/nginx/sites-enabled/
+sudo ln -s $project_path/systemd/$basedir.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
+sudo systemctl start gu-$basedir
+sudo systemctl enable gu-$basedir
 sudo service nginx restart
