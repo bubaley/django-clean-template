ba6064025bba6137aefa02751d14870df9eb0e8d
bubaley <bubaley.fu@gmail.com>
2020-04-28 16:17:44 +0000

    rebuild

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ read -p "Git url to your django-project: " git_url
 $base_python -m venv env
 source env/bin/activate
 pip install -U pip
-
+pip install gunicorn
 git clone $git_url src
 pip install -r src/requirements.txt
 
@@ -32,18 +32,35 @@ if [ -e "/etc/nginx/sites-enabled/$basedir.conf" ]
 then
 	echo "Ссылка для nginx уже создана"
 else
-	sudo ln -s /$project_path/nginx/$basedir.conf /etc/nginx/sites-enabled/
+	sudo ln -s $project_path/nginx/$basedir.conf /etc/nginx/sites-enabled/
 fi
 
 if [ -e "/etc/systemd/system/gu-$basedir.service" ]
 then
 	echo "Ссылка на сервис уже существует"
 else
-	sudo ln -s  /$project_path/systemd/gu-$basedir.service /etc/systemd/system/
+	sudo ln -s  $project_path/systemd/gu-$basedir.service /etc/systemd/system/
 fi
+
+random_str=`sudo head /dev/urandom | tr -dc "A-Za-z0-9!#$%&()*+,-./:;<=>?@[\]^_{|}~" | fold -w 50 | head -n 1`
+env_path="$project_path/src/core/settings/.env"
+echo "SECRET_KEY=$random_str" >> $env_path
+echo "ALLOWED_HOST=$project_domain"
 
 sudo systemctl daemon-reload
 sudo systemctl start gu-$basedir
 sudo systemctl enable gu-$basedir
 sudo service nginx restart
 
+setup_ssl=""
+read -p "Do you want to install ssl? (y/n): " setup_ssl
+if [ "$setup_ssl" = "y"  ]
+then
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot python-certbot-nginx
+sudo certbot --nginx
+fi
